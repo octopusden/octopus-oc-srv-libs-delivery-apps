57d37307e5b5c5317ef92f35d46defe47b1c52fa
ivansa-ru <ivansa@me.com>
2023-04-17 12:48:40 +0300

    Build wf (#1)
    * add build workflow
    * version increased

--- setup.py
+++ setup.py
@@ -3,19 +3,21 @@ from setuptools import setup
 import glob
 import os
 
-__version = "11.2.0.1"
+__version = "11.2.1"
 
 spec = {
-    'name': "oc-delivery-apps",
-    'version': __version, 
-    'description': 'Models and controllers for dlcontents application',
-    'license': "LGPLv2",
-    'packages': [
-        'oc_delivery_apps',
-        'oc_delivery_apps.checksums',
-        'oc_delivery_apps.dlcontents'],
-    'install_requires': [
-        "oc-sql-helpers >= 0.16.0.0",
+    "name": "oc-delivery-apps",
+    "version": __version, 
+    "description": "Models and controllers for dlcontents application",
+    "long_description": "",
+    "long_description_content_type": "text/plain",
+    "license": "LGPLv2",
+    "packages": [
+        "oc_delivery_apps",
+        "oc_delivery_apps.checksums",
+        "oc_delivery_apps.dlcontents"],
+    "install_requires": [
+        "oc-sql-helpers >= 1.0.0",
         "python-magic",
         "oc-orm-initializator",
         "sqlparse",
